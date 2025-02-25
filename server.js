392f18883fdf408a4e31db62529e5f481d790101
fbansept <bansept.franck@gmail.com>
2025-02-25 15:39:04 +0100

    add docker compose

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
 const express = require('express');
+const { createClient } = require('redis');
 const app = express();
 const port = 3000;
 
-app.get('/time', (req, res) => {
+// Remplacez 'your_redis_ip' et 'your_redis_port' par l'adresse IP et le port de votre serveur Redis
+const redisHost = 'db';
+const redisPort = 6379; // Remplacez par le port de votre serveur Redis si différent
+
+// Créer un client Redis avec l'adresse IP et le port spécifiés
+const redisClient = createClient({
+    url: `redis://${redisHost}:${redisPort}`
+});
+
+redisClient.on('error', (err) => {
+    console.error('Erreur Redis :', err);
+});
+
+redisClient.on('connect', () => {
+    console.log('Connecté à Redis');
+});
+
+// Connexion au client Redis
+redisClient.connect().catch(console.error);
+
+app.get('/time', async (req, res) => {
     const currentTime = new Date().toISOString();
-    res.send(`The current time is ${currentTime}`);
+
+    try {
+        // Enregistrer la date dans Redis
+        await redisClient.lPush('times', currentTime);
+        res.send(`The current time is ${currentTime}`);
+    } catch (error) {
+        res.status(500).send('Erreur lors de l\'enregistrement de la date');
+    }
+});
+
+app.get('/times', async (req, res) => {
+    try {
+        // Récupérer toutes les dates de Redis
+        const times = await redisClient.lRange('times', 0, -1);
+        res.send(times);
+    } catch (error) {
+        res.status(500).send('Erreur lors de la récupération des dates');
+    }
 });
 
 app.listen(port, () => {
